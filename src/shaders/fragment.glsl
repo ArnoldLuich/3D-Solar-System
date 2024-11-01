60453b239efdb8b2ccf57843e5264919edd07017
arnold <arnold.arney@gmail.com>
2024-11-01 18:26:25 +0200

    Add Planets

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,6 +1,16 @@
 uniform sampler2D uTexture;
+uniform sampler2D uBumpMap;
+uniform int uHasBumpMap;
 varying vec2 vUv;
 
 void main() {
-    gl_FragColor = texture2D(uTexture, vUv);
+    vec4 color = texture2D(uTexture, vUv);
+    float brightness = 1.0;
+    // If a bump map exists
+    if (uHasBumpMap == 1) {
+        vec4 bump = texture2D(uBumpMap, vUv);
+        float bumpScale = 0.9999; // Adjust this value to increase or decrease the effect
+        brightness = 0.99 + (bump.r - 0.5) * bumpScale;
+    }
+    gl_FragColor = vec4(color.rgb * brightness, color.a);
 }
