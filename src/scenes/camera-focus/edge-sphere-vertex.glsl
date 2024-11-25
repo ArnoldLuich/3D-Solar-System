e30f305be891c794bf10530e3f8799d933035ab1
user <hanspartelpani@gmail.com>
2024-11-25 05:38:45 +0200

    clickable html labels, protomolecule font

--- src/scenes/camera-focus/edge-sphere-vertex.glsl
+++ src/scenes/camera-focus/edge-sphere-vertex.glsl
@@ -7,6 +7,9 @@ void main() {
     vec3 n = normalize(normalMatrix * normal);
     vec3 v = normalize(n - p);
     diff = 1.0 - abs(dot(v, n));
+    if (diff < 0.5) {
+        diff = 0.0;
+    }
     
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
